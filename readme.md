63f4ef3107a8eeb4b4686c425b9a9ba0a2c92e15
JunPeng Jin <junpengjin@gmail.com>
2024-12-13 01:22:33 +0100

    Commit inicial supongo tehee

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Proyecto RT
+
+Proyecto de 2024 para aprender conceptos básicos sobre la tecnología de renderización "Ray Tracing" (o "Path tracing" más adelante).
+
+Se está siguiendo la serie de libros "Ray Tracing in one weekend", por lo tanto, ¡el crédito van a Peter Shirley, Trevor D Black, Steve Hollasch!
+
+Hay que tener en cuenta que se programa sin ninguna otra librería... así que no usa la GPU... ya se hará otro proyecto para eso...
+
+# RT Project
+
+2024 project to learn basic concepts about “Ray Tracing” rendering technology (or “Path tracing” later on).
+
+It is following the “Ray Tracing in one weekend” book series, so credit goes to Peter Shirley, Trevor D Black, Steve Hollasch!
+
+Note that it is programmed without any other library... so it doesn't use the GPU... another project will be done for that...
+
+# Referencias / references
+
+- “Ray Tracing in one weekend” - Peter Shirley, Trevor D Black, Steve Hollasch <https://raytracing.github.io/>
---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include "./class/vec3.hpp"
+#include "./class/ray.hpp"
+#include "./class/color.hpp"
+
+#include <iostream>
+
+color ray_color(const ray &r)
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+
+int main()
+{
+    // imagen
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // calcular la altura de la imagen
+
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // camera
+
+    auto focal_lenght = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // vectores del viewport
+
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // calcular el delta horizontal como vertical
+
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // calcular coordenadas del pixel arriba izquierda
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_lenght) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Render
+
+    std::cout
+        << "P3\n"
+        << image_width << " " << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++)
+    {
+        std::clog << "\rScanlines remaning: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++)
+        {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            auto pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+    std::clog << "\rDone.                 \n";
+}
