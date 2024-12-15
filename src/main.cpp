a06a05301f5110e0952b9bc319417f62f5bcadb8
JunPeng Jin <junpengjin@gmail.com>
2024-12-15 02:50:09 +0100

    clases camara y intervalo

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,10 @@
 #include "./class/hittable.hpp"
 #include "./class/hittables.hpp"
 #include "./class/sphere.hpp"
-
-color ray_color(const ray &r, const hittable &world)
-{
-    hit_record rec;
-
-    if (world.hit(r, 0, infinity, rec))
-    {
-        return 0.5 * (rec.normal + color(1, 1, 1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
+#include "./class/camera.hpp"
 
 int main()
 {
-    // imagen
-    auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
-
-    // calcular la altura de la imagen
-
-    int image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
     // mundo
 
     hittable_list world;
@@ -36,45 +14,10 @@ int main()
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    // camera
+    camera cam;
 
-    auto focal_lenght = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
 
-    // vectores del viewport
-
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
-
-    // calcular el delta horizontal como vertical
-
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // calcular coordenadas del pixel arriba izquierda
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_lenght) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-
-    // Render
-
-    std::cout
-        << "P3\n"
-        << image_width << " " << image_height << "\n255\n";
-
-    for (int j = 0; j < image_height; j++)
-    {
-        std::clog << "\rScanlines remaning: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++)
-        {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            auto pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
-        }
-    }
-    std::clog << "\rDone.                 \n";
+    cam.render(world);
 }
